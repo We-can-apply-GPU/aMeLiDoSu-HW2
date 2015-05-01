4421ee3fd7a8a90b015285f116aa471e19d25340
b02901088 <b02901088@linux8.csie.ntu.edu.tw>
2015-05-01 21:09:43 +0800

    hw1data to util

--- userCode/util.py
+++ userCode/util.py
@@ -64,3 +64,21 @@ def viterbi(x, w, y = []):
         now = trace[i][now]
         ans.append(now)
     return np.array(ans[::-1])
+def hw1data(f = 'data'):
+    d = open(f)
+    now=['people','talk']
+    ls = []
+    m = []
+    for i in d:
+        s = i.rstrip().split(',[')
+        n = s[0].split('_')
+        s = (s[1][:-1]).split(', ')
+        if now[0]!='people':
+            if now[0] != n[0] or now[1] != n[1]:
+                ls.append(np.array(m))
+                m = []
+        m.append([ float(i) for i in s])
+        now[0] = n[0]
+        now[1] = n[1]
+    ls.append(np.array(m))
+    return ls
