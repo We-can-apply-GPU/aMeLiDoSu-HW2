ba97590335ce6bf676b22533463aa18626ee67a8
b02901088 <b02901088@linux8.csie.ntu.edu.tw>
2015-05-01 21:08:26 +0800

    list of matrix

--- userCode/hw1data.py
+++ userCode/hw1data.py
@@ -1,10 +1,19 @@
 import numpy as np
 def hw1data(f = 'data'):
     d = open(f)
+    now=['people','talk']
+    ls = []
     m = []
     for i in d:
-        s = (i.rstrip().split())[1]
-        s = (s[1:-1]).split(',')
-        print(s)
+        s = i.rstrip().split(',[')
+        n = s[0].split('_')
+        s = (s[1][:-1]).split(', ')
+        if now[0]!='people':
+            if now[0] != n[0] or now[1] != n[1]:
+                ls.append(np.array(m))
+                m = []
         m.append([ float(i) for i in s])
-    return np.array(m)
+        now[0] = n[0]
+        now[1] = n[1]
+    ls.append(np.array(m))
+    return ls
