dbb783f9ee92ab937752efb063d1e0d3e4cf75be
b02901088 <b02901088@linux8.csie.ntu.edu.tw>
2015-05-06 22:12:11 +0800

    bug fixed

--- userCode/errorate.py
+++ userCode/errorate.py
@@ -1,11 +1,14 @@
+#!/usr/bin python3
 import numpy as np
+import sys
+
 def error(x,y):
     lx = x.__len__()
     ly = y.__len__()
     d = np.zeros((lx+1,ly+1))
-    for i in range(lx) : 
+    for i in range(lx+1) : 
         d[i][0] = i
-    for j in range(ly) : 
+    for j in range(ly+1) : 
         d[0][j] = j
     for j in range(1,ly+1):
         for i in range(1,lx+1):
@@ -25,8 +28,11 @@ def read(infile="./train.csv",ans="./ans.csv"):
         dic[line[0]] = line[1]
     for line in x:
         line = line.rstrip().split(",")
-        err += error(dic[line[0]],line[1])
-        reflen+=dic[line[0]].__len__()
+        if line[0]!='id' and line[0]!='ID':
+            err += error(dic[line[0]],line[1])
+            reflen+=dic[line[0]].__len__()
     return err/reflen
-
-print(read())
+if len(sys.argv) == 3:
+    print(read(sys.argv[1],sys.argv[2]))    
+else:
+    print(read())
